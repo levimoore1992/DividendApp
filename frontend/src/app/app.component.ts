7d784aab88fac0f66ef310a7d1538932fea83654
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-03 10:55:11 -0500

    frontend work

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {StockService} from './stock.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'Stock';
+  stockData: {name: string, shares_needed: number, investment_needed: number };
+  stockForm: FormGroup;
+  submitted = false;
+  loading = false;
+
+  constructor(private fb: FormBuilder, private stockService: StockService) {}
+
+  ngOnInit(): void {
+    this.stockForm = this.fb.group({
+      ticker: ['', Validators.required]
+    });
+  }
+
+  submit() {
+    this.submitted = true;
+    this.loading = true;
+    const payload = {
+      ticker: this.stockForm.get('ticker').value
+    };
+
+    this.stockService.postStock(payload).subscribe(res => {
+      // @ts-ignore
+      this.stockData = res;
+      this.loading = false
+    });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
