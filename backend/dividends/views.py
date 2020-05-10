8e3404b9cdcf2635e16ab1bb6e026d49a561466e
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-09 20:54:33 -0500

    Update views.py

--- backend/dividends/views.py
+++ backend/dividends/views.py
@@ -9,12 +9,14 @@ from .models import Stock
 class DividendViewSet(APIView):
 
     def post(self, request, *args, **kwargs):
+
         serializer = TickerSerializer(data=request.data)
         if serializer.is_valid():
             ticker = serializer.validated_data
         else:
             return Response({'error': 'Failed during serialization'})
         stock = yf.Ticker(ticker['ticker'])
+
         try:
             dividend_rate = float(stock.info['dividendRate'])
             price = float(stock.info['ask'])
@@ -26,10 +28,16 @@ class DividendViewSet(APIView):
         shares_needed = 1200 / dividend_rate
 
         investment_needed = shares_needed * price
-        stock_model = Stock(stock_name=name, dividend=dividend_rate,
-                            price=price, shares_needed=shares_needed,
-                            investment_needed=investment_needed)
-        stock_model.save()
+        stock_model, created = Stock.objects.update_or_create(stock_name=name,
+                                                              defaults={'dividend': dividend_rate,
+                                                                        'price': price,
+                                                                        'shares_needed': shares_needed,
+                                                                        'investment_needed': investment_needed})
+        if stock_model:
+            stock_model.save()
+        else:
+            created.save()
+
         return Response({'name': name, 'shares_needed': round(shares_needed),
                          'investment_needed': round(investment_needed)})
 
