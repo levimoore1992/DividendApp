ba3ae938d3af09537f3f6e48a3bbad941de4c5c8
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-12 20:06:16 -0500

    added management commands

---
+++ backend/core/management/commands/update_stocks.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from backend.dividends.models import Stock
+from backend.dividends.utils import get_stock_data
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        for stock in Stock.objects.all().values_list('ticker'):
+            try:
+                get_stock_data(stock[0])
+                self.stdout.write(f"Data written for {stock} ")
+
+            except:
+                continue
--- backend/dividends/cron.py
+++ backend/dividends/cron.py
@@ -4,10 +4,12 @@ from .models import Stock
 from background_task import background
 
 
-# @background(schedule=60)
+@background(schedule=60)
 def get_stock():
     for stock in Stock.objects.all().values_list('ticker'):
         try:
             get_stock_data(stock[0])
+            self.stdout.write(f"Data written for stock ")
+
         except:
             continue
