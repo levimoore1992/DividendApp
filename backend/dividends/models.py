49dbfaccc461a7748c17b7c04adb5b1fb727d3af
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-14 09:51:02 -0500

    edited model to include a field for of its investable

--- backend/dividends/models.py
+++ backend/dividends/models.py
@@ -7,6 +7,12 @@ class Stock(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     price = models.FloatField(round(2))
     ticker = models.CharField(max_length=4)
+    is_investable = models.BooleanField(default=False)
 
     def __str__(self):
         return self.stock_name
+
+    def save(self, *args, **kwargs):
+        if float(self.dividend) / float(self.price) > .05:
+            self.is_investable = True
+        super(Stock, self).save(*args, **kwargs)
