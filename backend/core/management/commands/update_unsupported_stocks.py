25ce2c995c65baa56cd290233b77b84fd387c9eb
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-14 19:53:31 -0500

    added unsupported stocks

---
+++ backend/core/management/commands/update_unsupported_stocks.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from dividends.models import UnsupportedStocks
+from dividends.utils import get_unsupported_stock_data
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+
+        for u_stock in UnsupportedStocks.objects.all().values_list('ticker'):
+            try:
+                get_unsupported_stock_data(u_stock[0])
+                self.stdout.write(f"Data written for {u_stock} ")
+            except:
+                continue
+        exit()
--- backend/dividends/utils.py
+++ backend/dividends/utils.py
@@ -1,5 +1,8 @@
-import yfinance as yf
+from sys import stdout
 
+import yfinance as yf
+import requests
+from bs4 import BeautifulSoup
 from .models import Stock, UnsupportedStocks
 
 
@@ -13,7 +16,6 @@ def get_stock_data(ticker):
     except Exception as e:
         unsupported = UnsupportedStocks(ticker=ticker)
         unsupported.save()
-        return {'error': f"This stock does not have all the info"}
 
     stock_model, created = Stock.objects.update_or_create(stock_name=name,
                                                           defaults={'dividend': dividend_rate,
@@ -23,5 +25,52 @@ def get_stock_data(ticker):
         stock_model.save()
     else:
         created.save()
-
     return {'stock_name': name}
+
+
+def get_dividend(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        if cols[0].find('strong').text == 'Annual Dividend:':
+            return float(cols[1].text.strip('$'))
+
+
+def get_price(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    div = soup.find('div', {'class': 'price'})
+    price_tag = div.find('strong')
+    price = float(price_tag.text.strip('$'))
+
+    return price
+
+
+def get_name(content):
+    soup = BeautifulSoup(content, 'html.parser')
+    h1 = soup.find('h1', {'class': 'PageTitleHOne'})
+    name = h1.contents[2].strip('Dividend Yield, History & Payout Ratio\n')
+    return name
+
+
+def get_unsupported_stock_data(ticker):
+    html = requests.get(f'https://www.marketbeat.com/stocks/NYSEARCA/{ticker}/dividend/')
+    content = html.content
+    dividend_rate = get_dividend(content)
+    price = get_price(content)
+    name = get_name(content)
+    try:
+
+        stock_model, created = Stock.objects.update_or_create(ticker=ticker,
+                                                          defaults={'dividend': dividend_rate,
+                                                                    'price': price,
+                                                                    'stock_name': name})
+    except Exception as e:
+        print(e)
+    if stock_model:
+        print(f'Stock Model {stock_model}')
+        stock_model.save()
+    else:
+        print(f'New: {created}')
+        created.save()
