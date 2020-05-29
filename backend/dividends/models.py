16d7cac8ca6779129a9343b28cd58ac4726fa14d
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-28 21:28:18 -0500

    added payment date

--- backend/dividends/models.py
+++ backend/dividends/models.py
@@ -13,32 +13,34 @@ class Stock(models.Model):
     is_owned = models.BooleanField(null=True, blank=True, default=False)
     shares_owned = models.IntegerField(default=0)
     ex_div_date = models.DateField(null=True, blank=True)
+    payment_date = models.DateField(null=True, blank=True)
     next_div_amount = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.ticker
 
     def save(self, *args, **kwargs):
-        self.ex_div_date, self.next_div_amount = self.get_ex_div_date(self.ticker)
+        self.ex_div_date, self.next_div_amount, self.payment_date = self.get_div_dates(self.ticker)
 
         self.is_investable = .07 < float(self.dividend) / float(self.price) < .13
 
         super(Stock, self).save(*args, **kwargs)
 
-    def get_ex_div_date(self, ticker):
+    def get_div_dates(self, ticker):
         r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=stocks')
         data = r.json()
         if data['data'] is None:
-            return None, None
+            return None, None, None
         elif data['data']['exDividendDate'] != 'N/A':
             date = datetime.strptime(data['data']['exDividendDate'], '%m/%d/%Y')
-            if date < datetime.now():
-                return None, None
+            payment_date = datetime.strptime(data['data']['dividendPaymentDate'], '%m/%d/%Y')
+            if payment_date < datetime.now():
+                return None, None, None
             next_div_amount = data['data']['dividends']['rows'][0]['amount'].strip('$')
             correct_date = date.strftime('%Y-%m-%d')
-            return correct_date, next_div_amount
+            return correct_date, next_div_amount, payment_date
         else:
-            return None, None
+            return None, None, None
 
 
 class UnsupportedStocks(models.Model):
