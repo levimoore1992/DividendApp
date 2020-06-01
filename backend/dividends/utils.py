5f32bf799022adb3bf9991afbf8159149151119f
levimoore1992 <levi.moore@exxonmobil.com>
2020-05-31 19:14:51 -0500

    refactored to use Nasdaq api

--- backend/dividends/utils.py
+++ backend/dividends/utils.py
@@ -1,26 +1,24 @@
 import yfinance as yf
 import requests
-from bs4 import BeautifulSoup
-from .models import Stock, UnsupportedStocks
+from .models import Stock
 
 
 def get_stock_data(ticker):
     stock = yf.Ticker(ticker)
+    try:
+        r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/info?assetclass=stocks').json()
+    except:
+        r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/info?assetclass=etf').json()
 
     try:
-        dividend_rate = float(stock.info['dividendRate'])
-        # price = float(stock.info['regularMarketPrice'])
-        price = float(stock.info['ask'])
-        name = stock.info['shortName']
+        price = r['data']['primaryData']['lastSalePrice'].strip('$')
+        name = r['data']['companyName']
     except Exception as e:
-        unsupported, created = UnsupportedStocks.objects.get_or_create(ticker=ticker)
-        if created:
-            created.save()
+
         return {'error':"Couldnt get this stock"}
 
     stock_model, created = Stock.objects.update_or_create(ticker=ticker,
-                                                          defaults={'dividend': dividend_rate,
-                                                                    'price': price,
+                                                          defaults={'price': price,
                                                                     'stock_name': name
                                                                     })
     if stock_model:
@@ -30,51 +28,7 @@ def get_stock_data(ticker):
     return {'stock_name': name}
 
 
-def get_dividend(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    table = soup.find('table')
-    rows = table.find_all('tr')
-    for row in rows:
-        cols = row.find_all('td')
-        if cols[0].find('strong').text == 'Annual Dividend:':
-            return float(cols[1].text.strip('$'))
 
-
-def get_price(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    div = soup.find('div', {'class': 'price'})
-    price_tag = div.find('strong')
-    price = float(price_tag.text.strip('$'))
-
-    return price
-
-
-def get_name(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    h1 = soup.find('h1', {'class': 'PageTitleHOne'})
-    name = h1.contents[2].strip('Dividend Yield, History & Payout Ratio\n')
-    return name
-
-
-def get_unsupported_stock_data(ticker):
-    html = requests.get(f'https://www.marketbeat.com/stocks/NYSEARCA/{ticker}/dividend/')
-    content = html.content
-    dividend_rate = get_dividend(content)
-    price = get_price(content)
-    name = get_name(content)
-    try:
-        stock_model, created = Stock.objects.update_or_create(ticker=ticker,
-                                                              defaults={'dividend': dividend_rate,
-                                                                        'price': price,
-                                                                        'name': name
-                                                                        })
-
-        if stock_model:
-            stock_model.save()
-        else:
-            created.save()
-    except Exception as e:
-        print(f'{ticker}: {e}')
 
 
 
