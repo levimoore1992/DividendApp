23389cc018458d9ef10d75e6e81d5d49fcfe67d9
levimoore1992 <levi.moore@exxonmobil.com>
2020-08-02 21:04:38 -0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,6 +1,6 @@
 import os
 import django_heroku
-djano_heroku.setting(locals())
+django_heroku.setting(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
