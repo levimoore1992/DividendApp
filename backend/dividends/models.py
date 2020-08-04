66208ce3e1b0e35d893835c7ae081df045f4d9ea
levimoore1992 <levi.moore@exxonmobil.com>
2020-08-04 09:16:29 -0500

    refactor

--- backend/dividends/models.py
+++ backend/dividends/models.py
@@ -18,55 +18,14 @@ class Stock(models.Model):
     headers = {
         'cookie': 'ak_bmsc=20E72A11270979763019E15FB14E0DCB1736A44F8C3800009BCE195FDF6A4B4D~pl5CLirohlLftep4KFVUCxjldV2kJZwe1phGpJFFxegNukmOHzREg3ps0f/bkMe48K8NL8M1GQhJLRPps5DlBmtG0BXuSN1RPUMY4hkS3gA00Bmc+0jdiXLAje7c1TeWloivWpGTkkSuaLhSsgkjaNlmVaqEA3unwR/nGYjKwUAPHxYR+BVedplusn+E/nZU5fitdSdtITr/cwHUEYLVMMlOpnCUzK28sMQ0C+FtF/V5g=; bm_sv=34A4184E3404304D5213DD0F2D5FBD7C~X4OG3OPNszlrUcbEwz8C3jwzbVNI0MUF86gjlFsxTBrwmykxFiQC/AY24CfItDE8ZIMJbSd1HLImRim8yvwWJaPFEdXFdpi7h+6NFnA291jSoDdPgltVQj3IEk1mRIiQkZc4zuJvDQbszYN3IfG4pgYmT26q5cYt4cxUM3ldXMI='
     }
+
     def __str__(self):
         return self.ticker
 
     def save(self, *args, **kwargs):
-        self.ex_div_date, self.next_div_amount, self.payment_date = self.get_div_dates(self.ticker)
         self.is_investable = .07 < float(self.dividend) / float(self.price) < .2
         if self.shares_owned > 0:
             self.is_owned = True
         else:
             self.is_owned = False
         super(Stock, self).save(*args, **kwargs)
-
-    def get_div_dates(self, ticker):
-
-
-        r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=stocks', headers=self.headers)
-        data = r.json()
-
-        if not data['data']:
-            return self.get_etf_data(ticker)
-
-        elif data['data']['exDividendDate'] != 'N/A':
-            ex_div_date = datetime.strptime(data['data']['exDividendDate'], '%m/%d/%Y')
-            payment_date = datetime.strptime(data['data']['dividendPaymentDate'], '%m/%d/%Y')
-            next_div_amount = data['data']['dividends']['rows'][0]['amount'].strip('$')
-            if ex_div_date < datetime.now():
-                return None, next_div_amount, payment_date
-            correct_date = ex_div_date.strftime('%Y-%m-%d')
-            return correct_date, next_div_amount, payment_date
-        else:
-            ex_div_date = datetime.strptime(data['data']['dividends']['rows'][0]['exOrEffDate'], '%m/%d/%Y')
-            payment_date = datetime.strptime(data['data']['dividends']['rows'][0]['paymentDate'], '%m/%d/%Y')
-            next_div_amount = data['data']['dividends']['rows'][0]['amount'].strip('$')
-            if ex_div_date < datetime.now():
-                return None, next_div_amount, payment_date
-            correct_date = ex_div_date.strftime('%Y-%m-%d')
-            return correct_date, next_div_amount, payment_date
-
-    def get_etf_data(self, ticker):
-        try:
-            r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=etf',headers=self.headers)
-            response = r.json()
-            data = response['data']['dividends']['rows'][0]
-            next_div_amount = data['amount'].strip('$')
-            try:
-                payment_date = datetime.strptime(data['paymentDate'], '%m/%d/%Y')
-            except:
-                payment_date = datetime.strptime(data['exOrEffDate'], '%m/%d/%Y')
-            return None, next_div_amount, payment_date
-        except Exception as e:
-            print(f'Failed in get etf function {self.ticker} {e}')
-            return None, None, None
--- backend/dividends/utils.py
+++ backend/dividends/utils.py
@@ -2,16 +2,21 @@ import requests
 from .models import Stock
 from datetime import datetime
 
+headers = {
+    'cookie': 'ak_bmsc=20E72A11270979763019E15FB14E0DCB1736A44F8C3800009BCE195FDF6A4B4D~pl5CLirohlLftep4KFVUCxjldV2kJZwe1phGpJFFxegNukmOHzREg3ps0f/bkMe48K8NL8M1GQhJLRPps5DlBmtG0BXuSN1RPUMY4hkS3gA00Bmc+0jdiXLAje7c1TeWloivWpGTkkSuaLhSsgkjaNlmVaqEA3unwR/nGYjKwUAPHxYR+BVedplusn+E/nZU5fitdSdtITr/cwHUEYLVMMlOpnCUzK28sMQ0C+FtF/V5g=; bm_sv=34A4184E3404304D5213DD0F2D5FBD7C~X4OG3OPNszlrUcbEwz8C3jwzbVNI0MUF86gjlFsxTBrwmykxFiQC/AY24CfItDE8ZIMJbSd1HLImRim8yvwWJaPFEdXFdpi7h+6NFnA291jSoDdPgltVQj3IEk1mRIiQkZc4zuJvDQbszYN3IfG4pgYmT26q5cYt4cxUM3ldXMI='
+}
+
 
 def get_stock_data(ticker):
-    headers = {'cookie': 'ak_bmsc=20E72A11270979763019E15FB14E0DCB1736A44F8C3800009BCE195FDF6A4B4D~pl5CLirohlLftep4KFVUCxjldV2kJZwe1phGpJFFxegNukmOHzREg3ps0f/bkMe48K8NL8M1GQhJLRPps5DlBmtG0BXuSN1RPUMY4hkS3gA00Bmc+0jdiXLAje7c1TeWloivWpGTkkSuaLhSsgkjaNlmVaqEA3unwR/nGYjKwUAPHxYR+BVedplusn+E/nZU5fitdSdtITr/cwHUEYLVMMlOpnCUzK28sMQ0C+FtF/V5g=; bm_sv=34A4184E3404304D5213DD0F2D5FBD7C~X4OG3OPNszlrUcbEwz8C3jwzbVNI0MUF86gjlFsxTBrwmykxFiQC/AY24CfItDE8ZIMJbSd1HLImRim8yvwWJaPFEdXFdpi7h+6NFnA291jSoDdPgltVQj3IEk1mRIiQkZc4zuJvDQbszYN3IfG4pgYmT26q5cYt4cxUM3ldXMI='}
     try:
         r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/info?assetclass=stocks', headers=headers).json()
         if r['data']:
-            d = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=stocks',headers=headers).json()
+            d = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=stocks',
+                             headers=headers).json()
         else:
             r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/info?assetclass=etf', headers=headers).json()
-            d = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=etf', headers=headers).json()
+            d = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=etf',
+                             headers=headers).json()
 
         name = r['data']['companyName']
         price = r['data']['primaryData']['lastSalePrice'].strip('$')
@@ -20,18 +25,24 @@ def get_stock_data(ticker):
         if dividend == "N/A":
             dividend = get_dividend(d)
 
+        ex_div_date, next_div_amount, payment_date = get_div_dates(ticker)
+
         stock_model, created = Stock.objects.update_or_create(ticker=ticker,
                                                               defaults={'price': price,
                                                                         'stock_name': name,
-                                                                        'dividend': dividend
+                                                                        'dividend': dividend,
+                                                                        'ex_div_date': ex_div_date,
+                                                                        'next_div_amount': next_div_amount,
+                                                                        'payment_date': payment_date
                                                                         })
         if stock_model:
             stock_model.save()
         else:
             created.save()
         return {'stock_name': name}
-    except:
-        raise Exception({'error': 'Couldnt return stock'})
+    except Exception as e:
+        raise Exception({'error': f'{e}'})
+
 
 def get_dividend(response_data):
     data = response_data['data']['dividends']['rows']
@@ -49,3 +60,44 @@ def get_dividend(response_data):
 
     else:
         return float(data[0]['amount'].strip('$')) * 2
+
+
+def get_div_dates(ticker):
+    r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=stocks', headers=headers)
+    data = r.json()
+
+    if not data['data']:
+        return get_etf_data(ticker)
+
+    elif data['data']['exDividendDate'] != 'N/A':
+        ex_div_date = datetime.strptime(data['data']['exDividendDate'], '%m/%d/%Y')
+        payment_date = datetime.strptime(data['data']['dividendPaymentDate'], '%m/%d/%Y')
+        next_div_amount = data['data']['dividends']['rows'][0]['amount'].strip('$')
+        if ex_div_date < datetime.now():
+            return None, next_div_amount, payment_date
+        correct_date = ex_div_date.strftime('%Y-%m-%d')
+        return correct_date, next_div_amount, payment_date
+    else:
+        ex_div_date = datetime.strptime(data['data']['dividends']['rows'][0]['exOrEffDate'], '%m/%d/%Y')
+        payment_date = datetime.strptime(data['data']['dividends']['rows'][0]['paymentDate'], '%m/%d/%Y')
+        next_div_amount = data['data']['dividends']['rows'][0]['amount'].strip('$')
+        if ex_div_date < datetime.now():
+            return None, next_div_amount, payment_date
+        correct_date = ex_div_date.strftime('%Y-%m-%d')
+        return correct_date, next_div_amount, payment_date
+
+
+def get_etf_data(ticker):
+    try:
+        r = requests.get(f'https://api.nasdaq.com/api/quote/{ticker}/dividends?assetclass=etf', headers=headers)
+        response = r.json()
+        data = response['data']['dividends']['rows'][0]
+        next_div_amount = data['amount'].strip('$')
+        try:
+            payment_date = datetime.strptime(data['paymentDate'], '%m/%d/%Y')
+        except:
+            payment_date = datetime.strptime(data['exOrEffDate'], '%m/%d/%Y')
+        return None, next_div_amount, payment_date
+    except Exception as e:
+        print(f'Failed in get etf function {ticker} {e}')
+        return None, None, None
